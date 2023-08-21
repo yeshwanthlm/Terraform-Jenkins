10d05132854c472b9f4608cf0c68b2a5d0ed2d2b
mail2diliprajput <87536229+mail2diliprajput@users.noreply.github.com>
2023-08-21 18:08:06 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "foo" {
-  ami           = "ami-05fa00d4c63e32376" # us-west-2
+  ami           = "ami-08a52ddb321b32a8c" 
   instance_type = "t2.micro"
   tags = {
       Name = "TF-Instance"
