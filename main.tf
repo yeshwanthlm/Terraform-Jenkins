f3faadde0569410fd2ee3408352045ddb85839fc
midhunraj007 <59755649+midhunraj007@users.noreply.github.com>
2023-09-28 10:40:26 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "foo" {
-  ami           = "ami-097300c6222ac2b2a" # us-west-2
+  ami           = "ami-097300c6222ac2b2a" # ca-central-1
   instance_type = "t2.micro"
   subnet_id     = "subnet-00529fad8ff2e503d"
   tags = {
