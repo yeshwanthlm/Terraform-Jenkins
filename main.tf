41432bf153a051b081b74277d6c5d55b6c60d01a
midhunraj007 <59755649+midhunraj007@users.noreply.github.com>
2023-09-30 09:08:04 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,8 +3,8 @@ provider "aws" {
   profile = "default"
 }
 
-resource "aws_instance" "foo" {
-  ami           = "ami-097300c6222ac2b2a" # ca-central-1
+resource "aws_instance" "example_server" {
+  ami           = "ami-0f0e79f25a393af23"
   instance_type = "t2.micro"
   subnet_id     = "subnet-0e2b335f2afe975fc"
   tags = {
