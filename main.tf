a43978f315237a7faba72019e1588f1a6028feb2
midhunraj007 <59755649+midhunraj007@users.noreply.github.com>
2023-09-30 09:00:59 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,12 @@
 provider "aws" {
-    region = "ca-central-1"  
+  region  = "ca-central-1"
+  profile = "default"
 }
 
 resource "aws_instance" "foo" {
   ami           = "ami-097300c6222ac2b2a" # ca-central-1
   instance_type = "t2.micro"
+  vpc_security_group_ids = "sg-03579e4db8b40b09f"
   subnet_id     = "subnet-00529fad8ff2e503d"
   tags = {
       Name = "TF-Instance"
