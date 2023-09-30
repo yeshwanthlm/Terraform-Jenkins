6bd49662f7571bac0fe4f582242655f4bb3c275d
midhunraj007 <59755649+midhunraj007@users.noreply.github.com>
2023-09-30 09:05:58 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,8 +6,7 @@ provider "aws" {
 resource "aws_instance" "foo" {
   ami           = "ami-097300c6222ac2b2a" # ca-central-1
   instance_type = "t2.micro"
-  vpc_security_group_ids = "sg-03579e4db8b40b09f"
-  subnet_id     = "subnet-00529fad8ff2e503d"
+  subnet_id     = "subnet-0e2b335f2afe975fc"
   tags = {
       Name = "TF-Instance"
   }
