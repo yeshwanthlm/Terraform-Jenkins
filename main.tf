508796c08db59c2ee4fc37a70a29d8fd40239314
midhunraj007 <59755649+midhunraj007@users.noreply.github.com>
2023-09-30 09:14:36 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,25 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }  
+  required_version = ">= 1.2.0"
+}
+
 provider "aws" {
   region  = "ca-central-1"
+  access_key = "AKIAT532D4I4P7VRD4PI"
+  secret_key = "tCnN/Ua8pRUYDNATnZ5Os6nR8IN7Yo9IN2S6mtNz"
   profile = "default"
 }
 
 resource "aws_instance" "example_server" {
   ami           = "ami-0f0e79f25a393af23"
   instance_type = "t2.micro"
-  subnet_id     = "subnet-0e2b335f2afe975fc"
-  tags = {
-      Name = "TF-Instance"
+
+    tags = {
+    Name = "midhunraj007@gmail.com"
   }
 }
