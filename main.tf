6a4640b5dd73e90dc13da70259fc665ea7d3743c
BharathRavula <120086788+Bharathravula007@users.noreply.github.com>
2023-10-12 21:51:54 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,10 +2,10 @@ provider "aws" {
     region = "us-east-1"  
 }
 
-resource "aws_instance" "foo" {
-  ami           = "ami-05fa00d4c63e32376" # us-west-2
+resource "aws_instance" "Suriya" {
+  ami           = "" # us-west-2
   instance_type = "t2.micro"
   tags = {
-      Name = "TF-Instance"
+      Name = "Bharath"
   }
 }
