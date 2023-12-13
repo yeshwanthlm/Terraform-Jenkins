ad90098a1120f12d01e5759ea4fd72d701f4247f
arun <arunsuresh66@gmail.com>
2023-12-13 12:23:37 +0530

    test1

--- main.tf
+++ main.tf
@@ -6,6 +6,6 @@ resource "aws_instance" "foo" {
   ami           = "ami-05fa00d4c63e32376" # us-west-2
   instance_type = "t2.micro"
   tags = {
-      Name = "TF-Instance"
+      Name = "test-instance1"
   }
 }
